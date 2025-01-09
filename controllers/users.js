5c2e6ae1d3a6cd73f3c45c4a918849f5955d2502
Hamza Ali <73743551+HamzaAliKundi@users.noreply.github.com>
2025-01-09 23:38:53 +0500

    fixes

--- controllers/users.js
+++ controllers/users.js
@@ -9,18 +9,23 @@ const users = asyncHandler(async (req, res) => {
     }
 
     const existingUser = await User.findOne({ email });
+    
     if (existingUser) {
-        return res.status(400).json({ message: "User with this email already exists." });
-    }
-
-    const user = await User.create(req.body);
-
-    if (user) {
-        return res.status(201).json({
-            message: "User created successfully",
-            user,
+        return res.status(200).json({
+            success: true,
+            message: "User with this email already exists.",
+            user: existingUser
         });
-    } else return res.status(500).json({ message: "Failed to create user." });
+    } else {
+        const user = await User.create(req.body);
+        if (user) {
+            return res.status(200).json({
+                success: true,
+                message: "User created successfully",
+                user
+            });
+        } else return res.status(500).json({ message: "Failed to create user." });
+    }
 });
 
 const getSingleUser = asyncHandler(async (req, res) => {
